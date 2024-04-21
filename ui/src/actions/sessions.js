872129ee278878b1dc32eb6019a473236d60831f
loren <lorenmichaelh@gmail.com>
2024-04-21 15:35:39 -0500

    add clerk, add session actions

---
+++ ui/src/actions/sessions.js
@@ -0,0 +1,26 @@
+export const loadSession = user => {
+  return (dispatch) => {
+    fetch("/me")
+    .then(r => {
+      if (r.ok) {
+        r.json().then(user => dispatch({type: "LOAD_SESSION", payload: user}))
+      } else {
+        r.json().then(data => console.log(data.errors))
+      }
+    })
+  }
+}
+
+export const createSession = user => {
+  return {
+    type: "CREATE_SESSION",
+    payload: user
+  }
+}
+
+export const deleteSession = () => {
+  return {
+    type: "DELETE_SESSION",
+    payload: null
+  }
+}
