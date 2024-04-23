20499dd72bba4560486a19b7bd73a8aeebefa5d3
loren <lorenmichaelh@gmail.com>
2024-04-23 18:36:06 -0500

    building out CRUD actions

--- api/index.js
+++ api/index.js
@@ -21,47 +21,104 @@ app.get('/', (req, res) => {
 
 // ITEMS
 app.get('/items', async (req, res) => {
-  res.send('Get Items!')
+  try {
+    const items = await fetchItems();
+    res.send(items)
+  } catch (err) {
+    res.status(400).send(`Error fetching items: ${err}`)
+  }
 })
 
 app.post('/items', async (req, res) => {
-  res.send('Post Items!')
+  try {
+    const item = req.body;
+    const response = await createItem(item);
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error creating item: ${err}`)
+  }
 })
 
 app.put('/items', async (req, res) => {
-  res.send('Update Items!')
+  try {
+    const item = req.body;
+    const response = await updateItem(item);
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error updating item: ${err}`)
+  }
 })
 
-app.delete('/items', async (req, res) => {
-  res.send('Delete Items!')
+app.delete('/items/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await deleteItem(id);
+    res.send(response)
+  } catch (err) {
+    res.status(400).send(`Error deleting item: ${err}`)
+  }
 })
 
 
 // CARTS
 app.get('/carts', async (req, res) => {
-  res.send('Get Carts!')
+  try {
+    const items = await fetchCarts();
+    res.send(items)
+  } catch (err) {
+    res.status(400).send(`Error fetching carts: ${err}`)
+  }
 })
 
 app.post('/carts', async (req, res) => {
-  res.send('Create Cart!')
+  try {
+    const cart = req.body;
+    const response = await createItem(cart);
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error creating cart: ${err}`)
+  }
 })
 
 app.put('/carts', async (req, res) => {
-  res.send('Update Cart!')
+  try {
+    const cart = req.body;
+    const response = await updateCart(cart);
+    res.send(response)
+  } catch (err) {
+    res.status(400).send(`Error updating cart: ${err}`)
+  }
 })
 
-app.delete('/carts', async (req, res) => {
-  res.send('Delete Cart!')
+app.delete('/carts/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await deleteCart(id);
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error deleting cart: ${err}`)
+  }
 })
 
 
 // ORDERS
 app.get('/orders', async (req, res) => {
-  res.send('Get Orders!')
+  try {
+    const items = await fetchOrders();
+    res.send(items)
+  } catch (err) {
+    res.status(400).send(`Error fetching orders: ${err}`)
+  }
 })
 
 app.post('/orders', async (req, res) => {
-  res.send('Create Order!')
+  try {
+    const cart = req.body;
+    const response = await createOrder(cart);
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error creating order: ${err}`)
+  }
 })
 
 // app.put('/orders', async (req, res) => {
