2019cf7e871dc30a423fa5a1426e82984d54f30f
loren <lorenmichaelh@gmail.com>
2024-04-23 14:54:43 -0500

    errors with clerk, removing code lines to implement later

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,21 +5,21 @@ import App from './App';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { ClerkProvider } from '@clerk/clerk-react';
+// import { ClerkProvider } from '@clerk/clerk-react';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY) {
-    throw new Error("Missing Publishable Key")
-}
+// if (!PUBLISHABLE_KEY) {
+//     throw new Error("Missing Publishable Key")
+// }
 
 const store = configureStore({ reducer: rootReducer });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    // <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <Provider store={store} >
             <App />
         </Provider>
-    </ClerkProvider>
+    // </ClerkProvider>
 );
