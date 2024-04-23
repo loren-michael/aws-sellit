93bfc62137d3dbab5b84e42d0fa295722c601efb
loren <lorenmichaelh@gmail.com>
2024-04-23 13:02:53 -0500

    navbar progress: renamed MUI bar, created new navbar comp with original code

--- ui/src/components/NavBar.js
+++ ui/src/components/NavBar.js
@@ -1,165 +1,51 @@
-import React, { useState } from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser, faArrowRightFromBracket, faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import logo from '../images/sellit-4.png'
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteSession } from '../actions/sessions';
 
-
-const pages = ['Home', 'See All'];
-const settings = ['Profile', 'Cart', 'Orders', 'Logout'];
 
 const NavBar = () => {
-  const [anchorElNav, setAnchorElNav] = useState(null)
-  const [anchorElUser, setAnchorElUser] = useState(null)  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {loggedIn, currentUser } = useSelector(store => store.sessions);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  function handleLogout() {
+    fetch('/logout', {
+      method: "DELETE"
+    })
+    .then(dispatch(deleteSession()))
+    .then(navigate('/'))
+  }
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  function handleCart() {
+    navigate("/cart")
+  }
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  
   return (
-    <div>
-      <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Sell It!
-          </Typography>
+    <div class="h-40">
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+      <div class="w-full">
+        <a href="/"><img href="/" class="h-36 float-left" src={logo} alt="Sell It! Home" /></a>
 
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Sell It!
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+        {loggedIn ? 
+          <span class="float-right" className="loginLinks">
+            <span class="font-sans font-medium text-lg pr-5 ">Welcome, {currentUser.username}! </span>
+            <NavLink to="/profile" href="/profile" class="font-sans text-lg text-black"><FontAwesomeIcon onClick={() => navigate("/profile")} icon={faCircleUser} size="xl" style={{color: "#000000", padding: "10px"}} /></NavLink>
+            <NavLink to="/cart" href="/cart" class="font-sans text-lg text-black"><FontAwesomeIcon onClick={() => handleCart()} icon={faCartShopping} size="xl" style={{color: "#000000", padding: "10px"}} /></NavLink>
+            <NavLink to="/" href="/" class="font-sans text-lg text-black"><FontAwesomeIcon onClick={() => handleLogout()} icon={faArrowRightFromBracket} size="xl" style={{color: "#000000", padding: "10px"}} /></NavLink>
+            {/* <FontAwesomeIcon icon={faBars} size="xl" style={{color: "#000000", padding: "10px"}} /> */}
+          </span>
+        :
+        <span class="float-right" className="loginLinks">
+            <NavLink to="/login">Log In</NavLink>  or  <NavLink to="/signup"> Sign Up </NavLink>
+            <FontAwesomeIcon icon={faCircleUser} size="xl" style={{color: "#000000",}} />
+          </span>
+      }
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>      
+      </div>
     </div>
   )
 }
