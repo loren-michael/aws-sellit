055e4ec8564a5e00d3c2e6505727918dcb100007
loren <lorenmichaelh@gmail.com>
2024-04-23 14:50:42 -0500

    adding clerk

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,12 +5,21 @@ import App from './App';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key")
+}
 
 const store = configureStore({ reducer: rootReducer });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store} >
-        <App />
-    </Provider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <Provider store={store} >
+            <App />
+        </Provider>
+    </ClerkProvider>
 );
